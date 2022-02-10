df92b2ba0002d2a3661ea5e4f3e973f16d3d860e
Leo Negre <34983000+L-e-N@users.noreply.github.com>
2022-02-10 12:20:49 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World, this is Leo writing a Python app!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8080)
